4736222d6c7f2b0b2e1c0e9bbfab4d5676ae5c64
jeonghyun <woose1390@gmail.com>
2022-11-24 09:25:10 +0900

    second commit

--- javascript/jquery.tubular.1.0.js
+++ javascript/jquery.tubular.1.0.js
@@ -87,15 +87,15 @@
                 pHeight, // player height, tbd
                 $tubularPlayer = $('#tubular-player');
 
-            // when screen aspect ratio differs from video, video must center and underlay one dimension
+            // // when screen aspect ratio differs from video, video must center and underlay one dimension
 
-            if (width / options.ratio < height) { // if new video height < window height (gap underneath)
-                pWidth = Math.ceil(height * options.ratio); // get new player width
-                $tubularPlayer.width(pWidth).height(height).css({left: (width - pWidth) / 2, top: 0}); // player width is greater, offset left; reset top
-            } else { // new video width < window width (gap to right)
-                pHeight = Math.ceil(width / options.ratio); // get new player height
-                $tubularPlayer.width(width).height(pHeight).css({left: 0, top: (height - pHeight) / 2}); // player height is greater, offset top; reset left
-            }
+            // if (width / options.ratio < height) { // if new video height < window height (gap underneath)
+            //     pWidth = Math.ceil(height * options.ratio); // get new player width
+            //     $tubularPlayer.width(pWidth).height(height).css({left: (width - pWidth) / 2, top: 0}); // player width is greater, offset left; reset top
+            // } else { // new video width < window width (gap to right)
+            //     pHeight = Math.ceil(width / options.ratio); // get new player height
+            //     $tubularPlayer.width(width).height(pHeight).css({left: 0, top: (height - pHeight) / 2}); // player height is greater, offset top; reset left
+            // }
 
         }
 
